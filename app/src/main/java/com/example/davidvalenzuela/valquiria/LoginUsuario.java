57a52355fb1be04925e15942d8a3c6482eb9ae60
David Valenzuela <davidevc.32@gmail.com>
2018-11-03 17:12:51 -0300

    1

--- app/src/main/java/com/example/davidvalenzuela/valquiria/LoginUsuario.java
+++ app/src/main/java/com/example/davidvalenzuela/valquiria/LoginUsuario.java
@@ -9,5 +9,6 @@ public class LoginUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_usuario);
+        getActionBar().hide();
     }
 }
