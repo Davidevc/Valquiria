3f5a131d5626b8ebf18d3c8af6de89f9a0c59ba6
David Valenzuela <davidevc.32@gmail.com>
2018-11-03 00:14:06 -0300

    Se agrega la clase usuario con sus respectivos campos.

---
+++ app/src/main/java/com/example/davidvalenzuela/valquiria/Clases/Usuario.java
@@ -0,0 +1,33 @@
+package com.example.davidvalenzuela.valquiria.Clases;
+
+public class Usuario {
+    int telefono;
+    String nombre;
+    String apellido;
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+
+}
