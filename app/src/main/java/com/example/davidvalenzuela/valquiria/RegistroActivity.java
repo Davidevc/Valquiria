dbc6c8db3bd86a82536e0d9a0790eb1345f9aca3
David Valenzuela <davidevc.32@gmail.com>
2018-11-08 09:08:28 -0300

    Mejoras al login y al registro. Inicio cambio de main activity.

--- app/src/main/java/com/example/davidvalenzuela/valquiria/RegistroActivity.java
+++ app/src/main/java/com/example/davidvalenzuela/valquiria/RegistroActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -21,11 +22,12 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class RegistroActivity extends AppCompatActivity implements Response.Listener<JSONObject>,Response.ErrorListener {
 
-    EditText telefono,nombre,apellido;
+    EditText telefono,nombre,apellido,contrasena,confirmarContrasena;
     Button registrar;
     ProgressDialog progreso;
 
@@ -37,34 +39,67 @@ public class RegistroActivity extends AppCompatActivity implements Response.List
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         telefono = (EditText) findViewById(R.id.etTelefono);
         nombre = (EditText) findViewById(R.id.etNombre);
         apellido = (EditText) findViewById(R.id.etApellido);
+        contrasena = (EditText) findViewById(R.id.etContrasena);
+        confirmarContrasena = (EditText) findViewById(R.id.etContrasenaConfirmar);
 
-        registrar = (Button) findViewById(R.id.btnRegistrar);
+        registrar = (Button) findViewById(R.id.btnSiguiente);
 
         request = Volley.newRequestQueue(this);
 
         registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (contrasena.getText().toString().equals(confirmarContrasena.getText().toString())){
                 cargarWebService();
+                }else{
+                     toastContrasena();
+                }
             }
         });
     }
 
-    private void cargarWebService() {
+   private void cargarWebService() {
         progreso = new ProgressDialog(this);
         progreso.setMessage("Cargando...");
         progreso.show();
 
+        if (validador()){
+
         String url="http://192.168.64.2/dbRemota/wsJSONRegistro.php?telefono="+telefono.getText().toString()
                 +"&nombre="+nombre.getText().toString()
-                +"&apellido="+apellido.getText().toString();
+                +"&apellido="+apellido.getText().toString()
+                +"&contrasena="+contrasena.getText().toString();
         url = url.replace(" ","%20");
 
         jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,url,null,this,this);
-        request.add(jsonObjectRequest);
+        request.add(jsonObjectRequest);}
+    }
+
+    private boolean validador() {
+        boolean b=false;
+        if (telefono.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Falta rellenar el Telefono ", Toast.LENGTH_SHORT).show();
+        }else if (telefono.getText().length()<9) {
+            Toast.makeText(this, "numero no valido\nintroduzca numero de 9 digitos\nejemplo: 9 1234 5678", Toast.LENGTH_LONG).show();}
+            else if (nombre.getText().toString().isEmpty()){
+                Toast.makeText(this, "Falta rellenar el nombre", Toast.LENGTH_SHORT).show();
+            } else if (apellido.getText().toString().isEmpty()) {
+                Toast.makeText(this, "Falta rellenar el apellido", Toast.LENGTH_SHORT).show();
+            } else if (contrasena.getText().toString().isEmpty()) {
+                Toast.makeText(this, "Falta rellenar la contraseña", Toast.LENGTH_SHORT).show();
+            }else if (confirmarContrasena.getText().toString().isEmpty()) {
+                Toast.makeText(this, "Falta rellenar confirmar contraseña", Toast.LENGTH_SHORT).show();
+            }
+                else{
+                b=true;
+            }
+    progreso.hide();
+    return b;
     }
 
 
@@ -76,10 +111,43 @@ public class RegistroActivity extends AppCompatActivity implements Response.List
 
     @Override
     public void onResponse(JSONObject response) {
-        Toast.makeText(this,"Registro completado",Toast.LENGTH_SHORT).show();
-        progreso.hide();
-        telefono.setText("");
-        nombre.setText("");
-        apellido.setText("");
+
+        try {
+            String validador = response.getString("validador");
+            if (validador.equals("true")){
+                progreso.hide();
+                Toast.makeText(this,"Registro completado",Toast.LENGTH_SHORT).show();
+                telefono.setText("");
+                nombre.setText("");
+                apellido.setText("");
+                contrasena.setText("");
+                confirmarContrasena.setText("");
+                progreso.hide();
+            }else if (validador.equals("false")) {
+                progreso.hide();
+                Toast.makeText(this, "Usuario ya existente!", Toast.LENGTH_SHORT).show();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void toastContrasena(){
+        Toast.makeText(this,"Error en el confirmar contraseña",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch(id){
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
