5fc3c6aff9c1d16c138504662652a35928ebd5e1
David Valenzuela <davidevc.32@gmail.com>
2018-11-13 06:59:52 -0300

    FIREBASE implementado y enviando mensajes.

--- app/src/main/java/com/example/davidvalenzuela/valquiria/Firebase/MiFirebaseMessagingService.java
+++ app/src/main/java/com/example/davidvalenzuela/valquiria/Firebase/MiFirebaseMessagingService.java
@@ -1,7 +1,17 @@
 package com.example.davidvalenzuela.valquiria.Firebase;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.media.app.NotificationCompat;
 import android.util.Log;
 
+import com.example.davidvalenzuela.valquiria.MainActivity;
+import com.example.davidvalenzuela.valquiria.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -13,9 +23,32 @@ public class MiFirebaseMessagingService extends FirebaseMessagingService {
         String from= remoteMessage.getFrom();
         Log.d("NOTICIAS", "Mensaje recibido de: "+ from);
 
-        if (remoteMessage.getNotification() !=null){
+        if (remoteMessage.getNotification() != null){
             Log.d("NOTICIAS", "Notificacion: "+ remoteMessage.getNotification().getBody());
 
+            mostrarNotificacion(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
         }
+        if(remoteMessage.getData().size() > 0){
+            Log.d("NOTICIAS","Data: "+remoteMessage.getData());
+        }
+    }
+
+    private void mostrarNotificacion(String title, String body) {
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+
+        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        Notification.Builder notificationBuilder = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.ic_alert)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setSound(soundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0,notificationBuilder.build());
     }
 }
--- app/src/main/java/com/example/davidvalenzuela/valquiria/MainActivity.java
+++ app/src/main/java/com/example/davidvalenzuela/valquiria/MainActivity.java
@@ -3,13 +3,29 @@ package com.example.davidvalenzuela.valquiria;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
+
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView infoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().subscribeToTopic("all");
+
+        infoTextView = (TextView) findViewById(R.id.tvInfo);
+
+        if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()){
+                String value = getIntent().getExtras().getString(key);
+                infoTextView.append("\n"+ key + ": "+value);
+            }
+        }
+
     }
 
     @Override
